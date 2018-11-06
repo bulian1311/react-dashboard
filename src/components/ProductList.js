2a2512610491cba9965a4672ef1f184493bf7f92
Nikolay Kachanov <bulian1311@gmail.com>
2018-11-06 14:51:59 +0300

    axios library added and first connect with magmer-api

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 
 import Item from './Item';
 
 const styles = theme => ({});
 
 class ProductList extends React.Component {
+  state = { products: [] };
+
+  async componentWillMount() {
+    try {
+      const res = await axios.get('http://magmer-api.herokuapp.com/product');
+      this.setState({ products: res.data });
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  listRender() {
+    return this.state.products.map(product => {
+      return (
+        <Item
+          key={product.url}
+          title={product.title}
+          description={product.description}
+        />
+      );
+    });
+  }
+
   render() {
     const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <Item title="Qwerty" description="xcvvvvvvv xcvvvvvvv" />
-      </div>
-    );
+    return <div className={classes.root}>{this.listRender()}</div>;
   }
 }
 
